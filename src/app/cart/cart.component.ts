35acd729fac74f35100ee7e0af0d22e8cba0d987
Kate Miháliková <kate@katemihalikova.cz>
2020-10-20 13:59:40 +0200

    Cart improvements

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { CartService } from '../cart.service';
 @Component({
@@ -11,6 +12,7 @@ export class CartComponent implements OnInit {
   items;
   checkoutForm;
   constructor(
+      private router: Router,
       private cartService: CartService,
       private formBuilder: FormBuilder,
   ) {
@@ -25,12 +27,14 @@ export class CartComponent implements OnInit {
     this.items = this.cartService.getItems();
   }
 
-  onSubmit(customerData) {
+  onSubmit(customerData: {name: string; address: string}) {
     // Process checkout data here
+
+    alert(`Your order of ${this.items.length} item${this.items.length === 1 ? '' : 's'} has been accepted and will be send to ${customerData.name}, ${customerData.address}.`);
+
     this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
-
-    console.warn('Your order has been submitted', customerData);
+    this.router.navigate(['/']);
   }
 
 }
